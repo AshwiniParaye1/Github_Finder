a30a5c68c667fca47e64edfa9a9c42a941c9b35a
Ashwini <ashwiniparaye9309@gmail.com>
2022-12-27 18:05:28 +0530

    modified alert

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import { GithubProvider } from "./context/github/GithubContext";
+import { AlertProvider } from "./context/alert/AlertContext";
 
 
 function App() {
   return (
     <GithubProvider>
+      <AlertProvider>
     <Router>
 
         <div className="flex flex-col justify-between h-screen">
@@ -32,6 +34,7 @@ function App() {
         </div>
            
     </Router>
+    </AlertProvider>
     </GithubProvider>
   );
 }
--- src/context/alert/alertContext.js
+++ src/context/alert/alertContext.js
@@ -1,5 +1,5 @@
 import { createContext, useReducer } from "react";
-import alertReducer from './alertReducer';
+import alertReducer from './AlertReducer';
 
 const AlertContext = createContext()
 
@@ -8,8 +8,20 @@ export const AlertProvider = ({children}) => {
     
     const [state, dispatch] = useReducer(alertReducer, initialState)
 
+    //set an alert
+    const setAlert = (msg, type) => {
+        dispatch({
+            type: 'SET_ALERT',
+            payload: {msg, type}
+        })
+
+        setTimeout(() => dispatch({type: 'REMOVE_ALERT'}), 3000)
+    }
+
+
+
     return (
-        <AlertContext.Provider value={{ alert: state }}>
+        <AlertContext.Provider value={{ alert: state, setAlert }}>
             {children}
         </AlertContext.Provider>
     )
