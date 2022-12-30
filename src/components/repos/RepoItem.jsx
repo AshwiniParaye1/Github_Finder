b40617277034e1e253f9017141b49a56e0313296
Ashwini <ashwiniparaye9309@gmail.com>
2022-12-30 15:12:31 +0530

    modified

--- src/components/repos/RepoItem.jsx
+++ src/components/repos/RepoItem.jsx
@@ -1,35 +1,40 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { FaEye, FaInfo, FaLink, FaStar, FaUtensils } from 'react-icons/fa';
+import { FaEye, FaInfo, FaLink, FaStar, FaUtensils } from 'react-icons/fa'
 
-function RepoItem({repo}) {
+import PropTypes from 'prop-types'
 
-    const {
-        name, description, html_url, forks, open_issues, watchers_count, stargazers_count,
-    } = repo
+function RepoItem({ repo }) {
+  const {
+    name,
+    description,
+    html_url,
+    forks,
+    open_issues,
+    watchers_count,
+    stargazers_count,
+  } = repo
 
   return (
-    <div className='mb-2 rounded-md card bg-gray-800 hover:bg-gray-900'>
+    <div className='mb-2 rounded-md card bg-base-200 hover:bg-base-300'>
       <div className='card-body'>
         <h3 className='mb-2 text-xl font-semibold'>
-            <a href={html_url} target="_blank" without rel="noreferrer">
-                <FaLink className='inline mr-1' /> {name}
-            </a>
+          <a href={html_url}>
+            <FaLink className='inline mr-1' /> {name}
+          </a>
         </h3>
         <p className='mb-3'>{description}</p>
         <div>
-            <div className='mr-2 badge badge-info badge-lg'>
-                <FaEye className='mr-2' /> {watchers_count}
-            </div>
-            <div className='mr-2 badge badge-success badge-lg'>
-                <FaStar className='mr-2' /> {stargazers_count}
-            </div>
-            <div className='mr-2 badge badge-error badge-lg'>
-                <FaInfo className='mr-2' /> {open_issues}
-            </div>
-            <div className='mr-2 badge badge-warning badge-lg'>
-                <FaUtensils className='mr-2' /> {forks}
-            </div>
+          <div className='mr-2 badge badge-info badge-lg'>
+            <FaEye className='mr-2' /> {watchers_count}
+          </div>
+          <div className='mr-2 badge badge-success badge-lg'>
+            <FaStar className='mr-2' /> {stargazers_count}
+          </div>
+          <div className='mr-2 badge badge-error badge-lg'>
+            <FaInfo className='mr-2' /> {open_issues}
+          </div>
+          <div className='mr-2 badge badge-warning badge-lg'>
+            <FaUtensils className='mr-2' /> {forks}
+          </div>
         </div>
       </div>
     </div>
@@ -37,8 +42,7 @@ function RepoItem({repo}) {
 }
 
 RepoItem.propTypes = {
-    repo: PropTypes.object.isRequired,
+  repo: PropTypes.object.isRequired,
 }
-
 
 export default RepoItem
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,101 +1,28 @@
-import { createContext, useReducer } from "react";
-import githubReducer from "./GithubReducer";
-
+import { createContext, useReducer } from 'react'
+import githubReducer from './GithubReducer'
 
 const GithubContext = createContext()
 
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+  }
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+  const [state, dispatch] = useReducer(githubReducer, initialState)
 
-export const GithubProvider = ({children}) => {
-    const initialState = {
-        users: [],
-        user: {},
-        loading: false,
-    }
-
-    const [state, dispatch] = useReducer(githubReducer, initialState)
-
-    //get search results
-    const searchUsers = async (text) => {
-        setLoading()
-
-        const params = new URLSearchParams({
-            q: text
-        })
-        
-        // fetching users data from github url
-        const response = await fetch(`${GITHUB_URL}/search/users?${params}`, 
-        {
-            headers: {
-                Authorization: `token ${GITHUB_TOKEN}`,
-            },
-        })
-
-        const {items} = await response.json()
-
-        dispatch({
-            type: 'GET_USERS',
-            payload: items,
-        })
-        
-    }
-
-    //get single user
-
-    const getUser = async (login) => {
-        setLoading()
-
-        console.log(login)
-
-        // fetching users data from github url
-        const response = await fetch(`${GITHUB_URL}/users/${login}`, 
-        {
-            headers: {
-                Authorization: `token ${GITHUB_TOKEN}`,
-            },
-        })
-
-        console.log(response);
-        if(response.status === 404){
-            window.location = '/notfound'
-        } else {
-            const data = await response.json()
-
-            dispatch({
-                type: 'GET_USER',
-                payload: data,
-                
-            })
-            
-        }
-
-
-        
-    }
-
-
-    //clear users from state
-    const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
-
-    //set loading
-    const setLoading = () => dispatch({type: 'SET_LOADING'})
-
-    return <GithubContext.Provider value={{
-        users: state.users,
-        loading: state.loading,
-        user: state.user,
-        searchUsers,
-        clearUsers,
-        getUser,
-        
-    }}>
-        
-        {children}
-        
+  return (
+    <GithubContext.Provider
+      value={{
+        ...state,
+        dispatch,
+      }}
+    >
+      {children}
     </GithubContext.Provider>
+  )
 }
 
-
-export default GithubContext;
+export default GithubContext
